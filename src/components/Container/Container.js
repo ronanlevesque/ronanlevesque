126298ddf1703fe457749ffac20311e13411d574
Ronan Levesque <ronan.levesque@algolia.com>
2019-05-07 11:58:02 +0200

    Fix package typo

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classNames';
+import cx from 'classnames';
 
 const Container = ({ children, className }) => (
   <div className={cx('m-auto maw-600 md:maw-800 lg:maw-1200', className)}>
