4d1873324cdc4e07b1ecb688eb1114d9ff821290
Ronan Levesque <ronan.levesque@algolia.com>
2019-05-07 12:50:26 +0200

    Remove unneeded deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,30 +11,6 @@ module.exports = {
     },
   },
   plugins: [
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     'gatsby-plugin-feed',
     {
       resolve: 'gatsby-plugin-google-fonts',
