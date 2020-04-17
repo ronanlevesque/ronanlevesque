9f6c6d4dae233b696e31821d84322b7ba72c219c
ronanlevesque <ronan.levesque@gmail.com>
2020-04-17 16:50:10 +0200

    Fix spacing

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,12 @@ import cx from 'classnames';
 
 const Hero = ({ alignLeft, children }) => (
   <header className="pv-80 fxg-1 d-flex md:pv-120 lg:pv-160">
-    <div className={cx('xs:w-90p m-auto', alignLeft ? 'ta-left' : 'ta-center')}>
+    <div
+      className={cx(
+        'ph-8 xs:w-90p xs:ph-0 m-auto',
+        alignLeft ? 'ta-left' : 'ta-center'
+      )}
+    >
       {children}
     </div>
   </header>
