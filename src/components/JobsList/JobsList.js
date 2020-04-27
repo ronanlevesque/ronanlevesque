51da9b03a1ab14fc7efebd26af04fed99e9aa309
ronanlevesque <ronan.levesque@gmail.com>
2020-04-27 08:46:52 +0200

    small fix

--- src/components/JobsList/JobsList.js
+++ src/components/JobsList/JobsList.js
@@ -8,8 +8,8 @@ const JobsList = ({ data }) => (
     {data.map(item => (
       <ListItem key={item.node.company}>
         <span className="d-flex jc-between ai-center fluid:pv-12-16 fluid:ph-8-12">
-          <p className="m-0 fxg-1 pr-12 fluid:fsz-14-18 sm:d-flex sm:ai-center">
-            <span className="d-block color-zircon fluid:w-200-260">
+          <p className="m-0 fxg-1 pr-4 fluid:fsz-14-18 sm:d-flex sm:ai-center">
+            <span className="d-block color-zircon fluid:w-180-260">
               {item.node.company}
             </span>
             <span className="d-block mt-4 fst-italic color-manatee sm:mt-0">
