a08b7cfc29164a31d2731361bb27d09e835008af
ronanlevesque <ronan.levesque@gmail.com>
2020-05-06 23:59:22 +0200

    Better Footer color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ import Svg from 'components/Svg';
 const Footer = ({ className, ...other }) => (
   <footer className={cx('fluid:pt-48-96 fluid:pb-20-40', className)} {...other}>
     <Svg
-      className="d-block m-auto h-auto color-manatee fluid:w-20-28"
+      className="d-block m-auto h-auto color-sanJuan fluid:w-20-28"
       name="logo-ronan-levesque"
     />
   </footer>
