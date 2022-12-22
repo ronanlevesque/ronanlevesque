22250d8eeaa692818e70dd517cfb743c9e718577
ronanlevesque <ronan.levesque@gmail.com>
2022-12-22 17:02:16 +0100

    fix: borders thumbnails

--- src/components/DrawingsList/DrawingsList.js
+++ src/components/DrawingsList/DrawingsList.js
@@ -5,20 +5,20 @@ import cx from 'classnames';
 const DrawingList = ({ drawings, folder, className, ...other }) => (
   <ul
     className={cx(
-      'm-0 d-grid pb-0 fluid:ph-8-12 fluid:pt-12-32 fluid:ggap-12-32 xs:g-2 sm:g-3 md:g-4 lg:g-6',
+      'm-0 d-grid pb-0 fluid:ph-8-12 fluid:pt-12-32 fluid:ggap-12-32 g-2 xs:g-3 sm:g-4 md:g-6',
       className
     )}
     {...other}
   >
     {drawings[folder].edges.map(drawing => (
       <a
-        className="td-none"
+        className="d-block td-none bds-solid bdw-1 bdc-white"
         href={`/images/drawing/${folder}/${drawing.node.name}.jpg`}
         key={drawing.node.name}
       >
         <li className="p-0 lis-none">
           <img
-            className="d-block w-100p h-auto bds-solid bdw-1 bdc-white"
+            className="d-block w-100p h-auto"
             src={`/images/drawing/${folder}/small/${drawing.node.name}-small.jpg`}
           />
         </li>
